b60ace5445e71302f0347d5c796e7c22503c7021
thisisjasonjafari <jasonjafari.m@gmail.com>
2020-05-07 02:25:13 -0400

    some changes

---
+++ src/sections/TitlesSection.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import Fullpage from "../components/Fullpage";
+import data from "../data.json";
+import { SocialIcon } from "react-social-icons";
+import "./TitlesSection.css";
+import DownIcon from "../components/DownIcon";
+// import { Link, Element } from "react-scroll";
+
+class TitlesSection extends Component {
+  constructor() {
+    super();
+    this.state = {
+      color: "white"
+    };
+    this.changeColor = this.changeColor.bind(this);
+  }
+
+  changeColor() {
+    this.setState({
+      color: this.state.color === "white" ? "yellow" : "white"
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Fullpage className="first">
+          <h1
+            className="title"
+            style={{
+              color: this.state.color
+            }}
+            onMouseOver={this.changeColor}
+            onMouseLeave={() => {
+              this.changeColor();
+            }}
+          >
+            {data.title}
+          </h1>
+          <div>
+            <h2>{data.subtitle}</h2>
+          </div>
+          <div className="icons-wrapper">
+            {Object.keys(data.links).map(key => {
+              return (
+                <div className="icon">
+                  <SocialIcon url={data.links[key]} />
+                </div>
+              );
+            })}
+          </div>
+        </Fullpage>
+        
+        
+      </div>
+    );
+  }
+}
+
+export default TitlesSection;
