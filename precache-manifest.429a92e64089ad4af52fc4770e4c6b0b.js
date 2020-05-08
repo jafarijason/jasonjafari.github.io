2c678f1e658ebe3267fd1f7db298d915b1289305
thisisjasonjafari <jasonjafari.m@gmail.com>
2020-05-08 13:47:09 -0400

    Updates

---
+++ precache-manifest.429a92e64089ad4af52fc4770e4c6b0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135b2c3e042a7567af85e61b47430a4a",
+    "url": "/JasonJafariResumeReact/index.html"
+  },
+  {
+    "revision": "f433985961160c840e5d",
+    "url": "/JasonJafariResumeReact/static/css/main.a79874f5.chunk.css"
+  },
+  {
+    "revision": "fba14e7a36b117829c78",
+    "url": "/JasonJafariResumeReact/static/js/2.c7b605b6.chunk.js"
+  },
+  {
+    "revision": "e4d0b7b6ee221c9758b548593a6bc2ac",
+    "url": "/JasonJafariResumeReact/static/js/2.c7b605b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f433985961160c840e5d",
+    "url": "/JasonJafariResumeReact/static/js/main.dea83d5c.chunk.js"
+  },
+  {
+    "revision": "7eb6c352420f69bb4215",
+    "url": "/JasonJafariResumeReact/static/js/runtime-main.9b10053e.js"
+  }
+]);
