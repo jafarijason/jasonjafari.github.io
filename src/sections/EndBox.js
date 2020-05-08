ed38d41a76138fcbcd7f5bae7e2471adad9ef6ad
thisisjasonjafari <jasonjafari.m@gmail.com>
2020-05-08 14:19:14 -0400

    last edit

--- src/sections/EndBox.js
+++ src/sections/EndBox.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
- 
+
 class EndBox extends Component {
   render() {
-    return <div name="end" />;
+    return <div name="end" >
+      <p className="fotterend" >Powerd by <a  className="afotterend" href={`https://github.com/thisisjasonjafari/JasonJafariResumeReact`} >Create React App</a> on GitHub</p>
+    </div>;
   }
 }
 
